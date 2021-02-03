4004b4dd28ecf99514cb4eec5250b70fb12ec62e
raycastillo2 <74738343+raycastillo2@users.noreply.github.com>
2021-02-03 15:25:33 -0500

    Game V1

---
+++ BananaPlatform/Assets/Manager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+    public static Manager instance;
+
+    public Transform respawnPoint;
+    public GameObject playerPrefab;
+
+
+
+    private void Awake() {
+        instance = this;
+    }
+    public void Respawn() {
+        GameObject Player = Instantiate(playerPrefab, respawnPoint.position, Quaternion.identity);
+
+    }
+    
+}
---
+++ BananaPlatform/Assets/playermovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playermovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+    public Animator animator;
+
+    float horizontalMove = 0f;
+
+    public float runSpeed = 50f;
+
+    bool jump = false;
+
+    void Update()
+    {
+
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+            animator.SetBool("IsJumping", true);
+
+        }
+
+    }
+
+    public void IsLanding () {
+        animator.SetBool("IsJumping", false);
+    }
+
+    void FixedUpdate () {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
+        
+    }
+}
